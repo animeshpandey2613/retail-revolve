2af4705c9ca7fc6d61d60570d6479d9568e7cfa6
animeshpandey2613 <121745585+animeshpandey2613@users.noreply.github.com>
2024-11-05 22:56:22 +0530

    connecting backend add products and edit products

--- src/Pages/AddProducts.jsx
+++ src/Pages/AddProducts.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Overlay from "../Components/AddProducts/Overlay";
 import Navbar from "../Components/SubComponents/Navbar";
 import Form from "../Components/AddProducts/Form";
+import axios from "axios";
 function AddProducts() {
   const [data, setData] = useState({
     productName: "",
@@ -15,15 +16,21 @@ function AddProducts() {
     specialFeature: "",
     liquidVolume: "",
     Description: "",
+    productType: "",
+    imageUrl: "",
   });
   const heading = "Add Product";
+  const SubmitHandler = async()=>{
+    const response = await axios("http://localhost:2020/finaldemo/AddProduct", data);
+    console.log(response);
+  }
   return (
     <div className="h-[200vh] relative ">
       <Overlay />
       <Navbar colorPrimary={"white"} colorSecondary={"white"} />
       <Form Data={data} Heading={heading} />
       <div className="absolute bottom-10 w-screen flex justify-center z-10">
-        <div className=" p-3 border-2 text-white rounded-2xl bg-[rgba(0,100,0,0.5)] cursor-pointer duration-500 hover:bg-[rgba(0,100,0,0.7)]">
+        <div onClick={SubmitHandler} className=" p-3 border-2 text-white rounded-2xl bg-[rgba(0,100,0,0.5)] cursor-pointer duration-500 hover:bg-[rgba(0,100,0,0.7)]">
           Add Product
         </div>
       </div>
--- src/Pages/EditProducts.jsx
+++ src/Pages/EditProducts.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Overlay from "../Components/AddProducts/Overlay";
 import Navbar from "../Components/SubComponents/Navbar";
 import Form from "../Components/AddProducts/Form";
+import axios from "axios";
 
 function EditProducts() {
   const [data, setData] = useState({
@@ -20,13 +21,17 @@ function EditProducts() {
     imageUrl:""
   });
   const heading = "Edit Product";
+  const SubmitHandler = async()=>{
+    const response = await axios("http://localhost:2020/finaldemo/UpdateProductDetails", data);
+    console.log(response);
+  }
   return (
     <div className="h-[200vh] relative ">
       <Overlay />
       <Navbar colorPrimary={"white"} colorSecondary={"white"} />
       <Form Data={data} Heading={heading} SetData = {setData} />
       <div className="absolute bottom-10 w-screen flex justify-center z-10">
-        <div className=" p-3 border-2 text-white rounded-2xl bg-[rgba(0,100,0,0.5)] cursor-pointer duration-500 hover:bg-[rgba(0,100,0,0.7)]">
+        <div onClick={SubmitHandler} className=" p-3 border-2 text-white rounded-2xl bg-[rgba(0,100,0,0.5)] cursor-pointer duration-500 hover:bg-[rgba(0,100,0,0.7)]">
           Edit Product
         </div>
       </div>
